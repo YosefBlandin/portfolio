df36841b75294b0e13b58728d679f97c1f566c1f
Yosef Enmanuel Blandin Rios <yosefleanb@gmail.com>
2022-06-15 16:16:38 -0400

    fix: button disabled background

--- styles/theme.ts
+++ styles/theme.ts
@@ -10,16 +10,23 @@ export const theme = extendTheme({
         maxWidth: { base: '270px', md: '300px' },
         width: '100%',
         marginTop: '20px',
-        _hover: { boxShadow: '0 0 0 0' }
+        _hover: { boxShadow: '0 0 0 0', }
       },
       variants: {
         primary: {
           backgroundColor: '#0070f3',
-          _hover: { boxShadow: '0 0 0 0' },
+          _hover: { boxShadow: '0 0 0 0', _disabled: {
+						background: '#0070f3'
+					} },
           boxShadow: '0 4px 14px 0 rgb(0 118 255 / 39%)'
         },
         secondary: {
           backgroundColor: '#000',
+					_hover: {
+						_disabled: {
+							background: '#000'
+						}
+					},
           boxShadow: '0 4px 14px 0 rgb(0 0 0 / 39%)'
         }
       }
