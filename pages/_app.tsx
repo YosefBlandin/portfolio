c75d20c13394673888f2a7af0ca8fd240a7e9cf7
Yosef Enmanuel Blandin Rios <yosefleanb@gmail.com>
2022-06-17 08:03:36 -0400

    disabled next auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,16 @@
 import '../styles/globals.css'
 import { AppProps } from 'next/app'
-import { SessionProvider } from 'next-auth/react'
+// import { SessionProvider } from 'next-auth/react'
 import { ChakraProvider } from '@chakra-ui/react'
 import { theme } from '../styles/theme'
 
 function MyApp ({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-		<SessionProvider session={session}>
+		// <SessionProvider session={session}>
 			<ChakraProvider theme={theme}>
 				<Component {...pageProps} />
 			</ChakraProvider>
-		</SessionProvider>
+		// </SessionProvider>
   )
 }
 
