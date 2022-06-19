242bdad8aa707bdb587a76d747053d6a82cfd448
Yosef Enmanuel Blandin Rios <yosefleanb@gmail.com>
2022-06-18 21:11:02 -0400

    Modifications to intersection observer

--- hooks/useIntersectionObserver.tsx
+++ hooks/useIntersectionObserver.tsx
@@ -1,59 +1,65 @@
 import { useState, useEffect, MutableRefObject } from 'react'
 
 interface iUseIntersectionObserverParams {
-  options: {
-    root: HTMLElement | null
-    rootMargin: string
-    threshold: number
-  }
-  target: MutableRefObject<null>
+	options: {
+		root: HTMLElement | null
+		rootMargin: string
+		threshold: number
+	}
+	target: MutableRefObject<null>
 }
 
 interface Bounds {
-  readonly height: number
-  readonly width: number
-  readonly top: number
-  readonly left: number
-  readonly right: number
-  readonly bottom: number
+	readonly height: number
+	readonly width: number
+	readonly top: number
+	readonly left: number
+	readonly right: number
+	readonly bottom: number
 }
 
 interface IntersectionObserverEntry {
-  readonly time: number
-  readonly rootBounds: Bounds
-  readonly boundingClientRect: Bounds
-  readonly intersectionRect: Bounds
-  readonly intersectionRatio: number
-  readonly target: Element
-  readonly isIntersecting: boolean
-  readonly isVisible: boolean
+	readonly time: number
+	readonly rootBounds: Bounds
+	readonly boundingClientRect: Bounds
+	readonly intersectionRect: Bounds
+	readonly intersectionRatio: number
+	readonly target: Element
+	readonly isIntersecting: boolean
+	readonly isVisible: boolean
 }
 
 export const useIntersectionObserver = ({
-  options,
-  target
+	options,
+	target,
 }: iUseIntersectionObserverParams) => {
-  const [entries, setEntries] = useState<
-    IntersectionObserverEntry | { isIntersecting: boolean }
-  >()
+	const [entries, setEntries] = useState<
+		IntersectionObserverEntry | { isIntersecting: boolean }
+	>()
 
-  const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => setEntries(entry))
-  }, options)
+	const observer =
+		typeof window !== 'undefined'
+			? new IntersectionObserver(entries => {
+					entries.forEach(entry => setEntries(entry))
+			  }, options)
+			: false
 
-  useEffect(
-    function currentElWatch () {
-      const targetCurrent = target.current
-      if (targetCurrent) {
-        observer.observe(targetCurrent)
-      }
-      return () => {
-        if (targetCurrent) observer.unobserve(targetCurrent)
-      }
-    },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    []
-  )
+	useEffect(
+		function currentElWatch () {
+			if (typeof window !== 'undefined') {
+				const targetCurrent = target.current
+				if (targetCurrent && observer !== false) {
+					observer?.observe(targetCurrent)
+				}
+				return () => {
+					if (targetCurrent && observer !== false)
+						observer?.unobserve(targetCurrent)
+				}
+			}
+		},
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+		[]
+	)
 
-  return entries
+	return entries
 }
