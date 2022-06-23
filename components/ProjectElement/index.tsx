5d5a5fd7ed5da819b1d56e608ff21382c826ed78
Yosef Enmanuel Blandin Rios <yosefleanb@gmail.com>
2022-06-23 18:43:25 -0400

    Improved the design of each project element

--- components/ProjectElement/index.tsx
+++ components/ProjectElement/index.tsx
@@ -1,3 +1,5 @@
+import { Box, Heading, Text } from '@chakra-ui/react'
+import Image from 'next/image'
 import Link from 'next/link'
 import { CSSProperties, FC } from 'react'
 import styles from './styles.module.css'
@@ -20,24 +22,18 @@ export const ProjectElement: FC<{
   return (
     <Link href={route} passHref >
       <a target='_blank' referrerPolicy='no-referrer'>
-			<article
-        style={{
-          ...style,
-          backgroundColor: backgroundColor
-        }}
-        className={styles.projectElementContainer}
-      >
-        <div
-          style={{
-            backgroundImage: `url(${backgroundImage})`
-          }}
-          className={styles.projectElementBackground}
-        ></div>
-        {/* <div className={styles.projectElementText}>
-          <h5>{projectName}</h5>
-          <p>Category: {projectCategory}</p>
-        </div> */}
-      </article>
+			<Box padding={'0'} textAlign={'center'} _hover={{ transform: 'scale(1.008)' }} transition='0.3s ease'>
+					<Image
+						style={{ borderRadius: '5px' }}
+						alt={projectName}
+						objectFit='cover'
+						width={300}
+						height={200}
+						src={backgroundImage}
+					/>
+          <Heading marginTop={'10px'} fontWeight={'bold'} fontSize={{ base: '1.1rem' }}>{projectName}</Heading>
+          <Text fontSize={{ base: '1rem' }}>{projectCategory}</Text>
+        </Box>
 			</a>
     </Link>
   )
